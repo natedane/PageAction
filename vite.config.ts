eae503fbd22d41ba948d5942584ca36455db8625
natedane <natedane@gmail.com>
2023-06-20 22:06:55 -0600

    env vars

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/PageAction/"
+export default defineConfig(({ command, mode})=>{
+  if ('build-production')
+  return{
+    plugins: [react()],
+    base: "./"
+  }
+  else
+    return{
+      plugins: [react()],
+      base: "/PageAction/"
+    }
 })
