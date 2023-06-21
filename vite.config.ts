366a3c95abec13bd91b40d78f9bb18ccc6eda43e
natedane <natedane@gmail.com>
2023-06-21 11:44:01 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode})=>{
-  if ('build-production')
+  if (mode == "prod")
   return{
     plugins: [react()],
     base: "./"
